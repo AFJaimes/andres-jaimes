828adfd58e6aa43c6128c27c4f21fbb6cab7285c
Andrés Felipe Jaimes Sánchez <andres_ja_sa@outlook.com>
2022-07-12 22:31:23 -0500

    refactor: clean code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3c8e316326e2e2f"; }
+/******/ 		__webpack_require__.h = function() { return "b67e3ddaa26554a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,5 +1,3 @@
-import createStyles from "@material-ui/core/styles/createStyles";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import { FunctionComponent } from "react";
 import experience from "data/experience.json";
 import css from "./Experience.module.scss";
@@ -7,45 +5,9 @@ import Header from "./Header/Header";
 import Job from "./Job/Job";
 import TimelineIcon from "./TimelineIcon/TimelineIcon";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      padding: theme.spacing(3),
-      textAlign: "justify",
-      backgroundColor: theme.palette.grey[900],
-      color: theme.palette.background.paper,
-    },
-    content: {
-      paddingLeft: theme.spacing(2),
-    },
-    subContent: {
-      paddingLeft: theme.spacing(4),
-    },
-    paper: {
-      backgroundColor: theme.palette.grey[800],
-      padding: "6px 16px",
-      color: "white",
-    },
-    secondaryTail: {
-      backgroundColor: theme.palette.secondary.main,
-    },
-    datesText: {
-      color: theme.palette.primary.light,
-    },
-    experienceTitle: {
-      color: theme.palette.secondary.light,
-    },
-    chargeText: {
-      color: theme.palette.secondary.main,
-    },
-  })
-);
-
 const Experience: FunctionComponent = () => {
-  const classes = useStyles();
-
   return (
-    <section className={classes.root}>
+    <section className={css.root}>
       <section className={css.container}>
         {experience.map((job) => {
           return (
@@ -57,8 +19,15 @@ const Experience: FunctionComponent = () => {
                   job={job.jobTitle}
                 />
               </div>
-              <div className={css.icon}><TimelineIcon icon={job.iconPath} /></div>
-              <div className={css.job}><Job description={job.jobDescription} jobTitle={job.companyName} /></div>
+              <div className={css.icon}>
+                <TimelineIcon icon={job.iconPath} />
+              </div>
+              <div className={css.job}>
+                <Job
+                  description={job.jobDescription}
+                  jobTitle={job.companyName}
+                />
+              </div>
             </div>
           );
         })}
